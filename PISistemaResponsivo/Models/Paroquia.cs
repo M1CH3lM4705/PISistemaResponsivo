01d21f4dc25ef995897ea7f0c884a103f4e1c873
Michel <michel.mef@gmail.com>
2020-11-20 17:46:55 -0400

    Adicionando validadores no lado do servidor na classe paroquia e voluntario

--- PISistemaResponsivo/Models/Paroquia.cs
+++ PISistemaResponsivo/Models/Paroquia.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace PISistemaResponsivo.Models
 {
-    [Table("Paroquias")]
+    [Table("Paróquias")]
     public class Paroquia
     {
-
+        [Key]
         public long ParoquiaId { get; set; }
+
+        [Required(ErrorMessage = "O digite o nome da paróquia.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Números e caracteres especiais não são permitidos no nome.")]
+        [Column("Nome", TypeName = "VARCHAR")]
         public string NomeParoquia { get; set; }
         public Endereco Endereco { get; set; }
 
--- PISistemaResponsivo/Models/Voluntario.cs
+++ PISistemaResponsivo/Models/Voluntario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,13 @@ namespace PISistemaResponsivo.Models
 {
     [Table("Voluntarios")]
     public class Voluntario
-    {
+    {   
+        [Key]
         public long VoluntarioId { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Números e caracteres especiais não são permitidos no nome.")]
+        [Column("Nome", TypeName = "VARCHAR")]
         public string NomeVoluntario { get; set; }
         public Endereco Endereco { get; set; }
     }
