ecbea80b28a12021a70fca4c41331453e8ef116b
Michel <michel.mef@gmail.com>
2020-11-21 21:30:16 -0400

    Criando as classes de persistenica

---
+++ PISistemaResponsivo/Infraestrutura/Dao/PessoaCarenteDao.cs
@@ -0,0 +1,24 @@
+﻿using PISistemaResponsivo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PISistemaResponsivo.Infraestrutura.Dao
+{
+    public class PessoaCarenteDao : DaoBase
+    {
+        public void Salvar(PessoaCarente pessoaCarente)
+        {
+            _contexto.PessoasCarentes.Add(pessoaCarente);
+            _contexto.SaveChanges();
+        }
+
+        public PessoaCarente Buscar(int pCarenteId)
+        {
+            return _contexto.PessoasCarentes.FirstOrDefault(x => x.CarenteId == pCarenteId);
+        }
+
+
+    }
+}
--- PISistemaResponsivo/Models/Endereco.cs
+++ PISistemaResponsivo/Models/Endereco.cs
@@ -12,13 +12,33 @@ namespace PISistemaResponsivo.Models
     {
         [Key]
         public long EnderecoId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "VARCHAR")]
         public string Rua { get; set; }
-        public int Numero { get; set; }
+
+        [RegularExpression(@"^[0-9]$")]
+        [MaxLength(5)]
+        [StringLength(5, MinimumLength = 3, ErrorMessage = "O número não pode ser menor que 2")]
+        [Column(TypeName = "VARCHAR")]
+        public string Numero { get; set; }
 
         [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Cep Incompleto")]
+        [DataType(DataType.PostalCode)]
+        [DisplayFormat(DataFormatString = "99999-999", ApplyFormatInEditMode = true)]
+        [Column(TypeName = "VARCHAR")]
         public string Cep { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [RegularExpression(@"^[a-zA-Z0-9 '' - '\ s] * $", ErrorMessage = "Caracteres espciais não são aceitos")]
         public string Bairro { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9 '' - '\ s] + $", ErrorMessage = "Caracteres espciais não são aceitos")]
+        [Column(TypeName = "VARCHAR")]
         public string Cidade { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
         public string Estado { get; set; }
     }
 }
